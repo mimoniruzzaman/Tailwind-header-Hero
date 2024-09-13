f09de07ad1b2e00562181451b731d85c04121d41
mimoniruzzaman <mimoniruzzaman43@gmail.com>
2024-09-13 07:00:56 -0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    // colors: {
+    //   heading: "#le293b",
+    //   primary: "#3a83f6",
+    //   secondary: "#8balf0",
+    //   white: "#ffffff",
+    // },
+    extend: {},
+  },
+  plugins: [],
+};
